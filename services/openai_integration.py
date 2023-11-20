aef1bf664d517db139852ee6c7ed543140da3734
mpnakamura <sny01.mzk10.lv@gmail.com>
2023-11-20 21:24:21 +0900

    better

--- services/openai_integration.py
+++ services/openai_integration.py
@@ -5,14 +5,14 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 category_prompts = {
-    "投資詐欺": "多いとされる投資詐欺を羅列して、解説ください。必ず280文字以内で返信してください。",
-    "金銭請求": "多いとされる金銭請求の詐欺を羅列して解説してください。必ず280文字以内で返信してください。",
-    "還付金詐欺": "多いとされる還付金詐欺を羅列して解説してください。必ず280文字以内で返信してください。",
+    "投資詐欺": "多いとされる投資詐欺を羅列して、解説ください。必ず500文字以内で返信してください。",
+    "金銭請求": "多いとされる金銭請求の詐欺を羅列して解説してください。必ず500文字以内で返信してください。",
+    "還付金詐欺": "多いとされる還付金詐欺を羅列して解説してください。必ず500文字以内で返信してください。",
     # 他のカテゴリのためのプロンプトもここに追加
 }
 
 # デフォルトのプロンプト
-default_prompt = "フレンドリーでエンゲージメントの高いトーンで、わかりやすい内容で、必ず280文字以内で応答してください。"
+default_prompt = "親切で丁寧な口調で情報をわかりやすく説明してください。流行語や専門用語は避け、シンプルで明確な言葉を使いましょう。また返答には必ず500文字以内で応答してください。"
 
 
 
@@ -24,9 +24,9 @@ def generate_response(user_message, category_selected):
             custom_prompt = default_prompt + "\n" + user_message
 
         response = openai_client.chat.completions.create(
-            model="gpt-4-1106-preview",
+            model="gpt-3.5-turbo-1106",
             messages=[{"role": "system", "content": custom_prompt}],
-            max_tokens=300,
+            max_tokens=500,
             temperature=0.7,
             top_p=1
         )
