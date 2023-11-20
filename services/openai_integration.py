9fe2c7d8a0a0237b80d53a532ed46865be1c3ac0
mpnakamura <sny01.mzk10.lv@gmail.com>
2023-11-20 13:04:56 +0900

    better

--- services/openai_integration.py
+++ services/openai_integration.py
@@ -4,17 +4,21 @@ import os
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
-def generate_response(user_message):
-    category_prompts = {
+
+def generate_response(user_message, prompt_type):
+    if prompt_type == "default":
+        # デフォルトプロンプト
+        custom_prompt = "フレンドリーでエンゲージメントの高いトーンで、わかりやすい内容で、必ず280文字以内で応答してください。"
+    else:
+        category_prompts = {
     "投資詐欺": "多いとされる投資詐欺を羅列して、解説ください。必ず280文字以内で返信してください。",
     "金銭請求": "多いとされる金銭請求の詐欺を羅列して解説してください。必ず280文字以内で返信してください。",
     "還付金詐欺": "多いとされる還付金詐欺を羅列して解説してください。必ず280文字以内で返信してください。",
     # 他のカテゴリのためのプロンプトもここに追加}
     }
-
-    custom_prompt = category_prompts.get(user_message, 
-        "フレンドリーでエンゲージメントの高いトーンで、わかりやすい内容で、必ず280文字以内で応答してください。"
-    )
+        custom_prompt = category_prompts.get(user_message, 
+            "フレンドリーでエンゲージメントの高いトーンで、わかりやすい内容で、必ず280文字以内で応答してください。"
+        )
 
     response = openai_client.chat.completions.create(
         model="gpt-4-1106-preview",
