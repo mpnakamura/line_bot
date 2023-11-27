f07eb83ec5597e4df9a9f9cddd42c6b2e7fcca39
mpnakamura <sny01.mzk10.lv@gmail.com>
2023-11-27 23:06:10 +0900

    be

--- handlers/reminder_scheduler.py
+++ handlers/reminder_scheduler.py
@@ -11,13 +11,13 @@ import logging
 logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = ['postgres://wlrtmqkbpdsrfi:e06b9de0d8df0366b33a6c2c2b7d8d45a4dfcee6fcc292ca2c6f4bece3aabdc5@ec2-44-213-151-75.compute-1.amazonaws.com:5432/d5vv1mhvjkkcvs']
 
 def get_db_connection():
     return psycopg2.connect(DATABASE_URL, sslmode='require')
 
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("O6HGHHmaa93mBBb20wRYdb8px6Uohi3XSghfxfTHYRr8TkUwMokQ7lWXChAc1Dr6vrOMD9BxUQ2K8SMgs2oPZAqI+i3BMgz/SUG++a6zmR84CFxgJ6imWCGWVaB0NFhYiSxjNcp07IeDC6OODpLLTwdB04t89/1O/w1cDnyilFU=")
+line_bot_api = LineBotApi("67ba629af8822b82d05afaa4624a2924")
 
 def send_reminders():
     conn = None
@@ -26,16 +26,14 @@ def send_reminders():
         with conn.cursor() as cursor:
             # Asia/Tokyoの現在時刻を取得
             tokyo_now = pytz.timezone('Asia/Tokyo').localize(datetime.now())
-            start_time = tokyo_now - timedelta(minutes=1)
-            end_time = tokyo_now + timedelta(minutes=1)
 
-            # 検索範囲の時刻をログに記録
-            logging.debug(f"Searching for reminders between {start_time} and {end_time}")
+            # 現在時刻より前に保存されているリマインダーを検索
+            logging.debug(f"Searching for reminders before {tokyo_now}")
 
             cursor.execute("""
                 SELECT reminder_id, user_id, details FROM UserSelections
-                WHERE datetime BETWEEN %s AND %s;
-            """, (start_time, end_time))
+                WHERE datetime <= %s;
+            """, (tokyo_now,))
             reminders = cursor.fetchall()
 
             # 検索されたリマインダーの詳細をログに記録
@@ -43,7 +41,7 @@ def send_reminders():
                 for reminder in reminders:
                     logging.debug(f"Reminder found: id={reminder[0]}, user_id={reminder[1]}, details={reminder[2]}")
             else:
-                logging.debug("No reminders found in the specified time range.")
+                logging.debug("No reminders found before the current time.")
 
             send_reminder_messages(reminders, cursor)
             delete_sent_reminders(reminders, cursor)
