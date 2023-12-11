662a4e1c441c5ea27a1c34d60477956bfb612694
mpnakamura <sny01.mzk10.lv@gmail.com>
2023-12-11 13:15:24 +0900

    btter

--- google_speech_to_text.py
+++ google_speech_to_text.py
@@ -1,10 +1,15 @@
 from google.cloud import speech
+import os
+
+# 環境変数からサービスアカウントキーのパスを取得
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 def convert_speech_to_text(audio_content):
     client = speech.SpeechClient()
     audio = speech.RecognitionAudio(content=audio_content)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
         language_code="ja-JP"
     )
 
