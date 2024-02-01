72881ba9dc76b037af96e8175fe7834279e2abbd
mpnakamura <sny01.mzk10.lv@gmail.com>
2024-02-01 13:23:18 +0900

    beter

--- services/chat.py
+++ services/chat.py
@@ -3,7 +3,7 @@ import re
 from llama_index.readers import BeautifulSoupWebReader
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
-from google_api_two import search_google
+from .google_api_two import search_google
 
 from urllib.parse import urlparse
 
